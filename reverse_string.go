33a57cb4b18cfd718bcc0c9d6ebbe167c0e30870
zhnbvsst10 <63463825+zhnbvsst10@users.noreply.github.com>
2022-05-13 10:45:40 +0600

    Update reverse_string.go

--- reverse_string.go
+++ reverse_string.go
@@ -2,5 +2,15 @@ package reverse_string
 
 func ReverseString(input string) (output string) {
 	// solution goes here
-	return output
+	output := []rune(s) // convert to rune
+    	for i, j := 0, len(output)-1; i < j; i, j = i+1, j-1 {
+  
+        // swap the letters of the string,
+        // like first with last and so on.
+        output[i], output[j] = output[j], output[i]
+    	}
+  
+    	// return the reversed string.
+   	// return string(rn)
+	return string(output)
 }
